5b2c282a4e00d55c26f9e9777e5243ad4f06ed6f
mattwandcow <mattwandcow@yahoo.com>
2021-01-20 22:07:50 -0700

    Javascript moved to external file

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -2,3 +2,16 @@ function toggleMenu()
 {
 document.getElementById("menu_list").classList.toggle("hide");
 }
+
+function displayDate()
+{
+    var d = new Date();
+    var weekday = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+        'November', 'December'
+    ];
+    document.getElementById('current_date').innerHTML = weekday[d.getDay()] + ", " + d.getDate() + " " + month[d
+        .getMonth()] + " " + d.getFullYear();
+    var dateObject = new Date();
+    document.getElementById('copy_year').innerHTML = d.getFullYear();
+}
